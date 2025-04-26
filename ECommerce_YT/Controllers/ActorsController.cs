1ad8a0359d293d53d85687d9aa8802cf47610e95
AGaffer <Ahmed.gomaa@el-eng.menofia.edu.eg>
2025-04-26 04:36:00 +0300

    Day 4

---
+++ ECommerce_YT/Controllers/ActorsController.cs
@@ -0,0 +1,23 @@
+﻿using ECommerce_YT.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce_YT.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
